f8d63dfe2708747659eaf53334bec1728779c544
Alexanne Brunold <alexanne.brunold@hetic.net>
2020-10-29 09:09:14 +0100

    maj course

---
+++ src/components/Title/Title.js
@@ -0,0 +1,23 @@
+import { useEffect } from 'react';
+
+function Title(props) {
+  const { title } = props;
+
+  useEffect(() => {
+    document.title = title;
+  }, [title]);
+
+  useEffect(() => {
+    function clickWindow() {
+      console.log(title);
+    }
+    window.addEventListener('click', clickWindow);
+    return () => {
+      window.removeEventListener('click', clickWindow);
+    };
+  }, [title]);
+
+  return <h1>{title}</h1>;
+}
+
+export default Title;
